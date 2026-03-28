f16d71d7e0bc5b8dc164d046d73b4d552b13e37f
dannyelkabli-design <dannyelkabli@gmail.com>
2026-03-28 19:25:58 +0100

    feat: assemble full homepage with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import { HeroSection } from '@/components/sections/HeroSection'
+import { StatBar } from '@/components/sections/StatBar'
+import { DienstenSection } from '@/components/sections/DienstenSection'
+import { ResultatenSection } from '@/components/sections/ResultatenSection'
+import { CoachSection } from '@/components/sections/CoachSection'
+import { PrijzenSection } from '@/components/sections/PrijzenSection'
+import { CTABanner } from '@/components/sections/CTABanner'
+import { ContactSection } from '@/components/sections/ContactSection'
+import { Footer } from '@/components/Footer'
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
+      <StatBar />
+      <DienstenSection />
+      <ResultatenSection />
+      <CoachSection />
+      <PrijzenSection />
+      <CTABanner />
+      <ContactSection />
+      <Footer />
     </main>
   )
 }
